c7f55c7dbd440b8ecaf192b12ec5e3402200875b
Divyam Rastogi <divyamr@flock.com>
2018-03-26 00:11:33 +0530

    Demo for the bug.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    {{ Star }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Star from "./assets/star.svg";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      Star
+    };
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
